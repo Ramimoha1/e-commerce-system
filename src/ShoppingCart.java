c18f189eade30ad0acf2f253faa761bdbd5e6b6f
Ivor Barrie Jaffery <hivor234@gmail.com>
2025-06-16 16:02:49 +0800

    unfinished implementation of customer, shoppingcart and user

---
+++ src/ShoppingCart.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private ArrayList<CartItem> items;
+
+    public ShoppingCart() {}
+
+    public void addToCart() {
+
+    }
+
+    public void removeItem() {
+
+    }
+
+    public void updateItemQuantity() {
+
+    }
+
+    public void viewCart() {
+
+    }
+
+    public double calculateTotal() {
+        double total = 0.0;
+        // implementation
+        return total;
+    }
+
+    public void clear() {
+
+    }
+}
